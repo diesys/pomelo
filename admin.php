1a873cae4e72efc11a321968ae383bbdaacfb297
sbiego <forsesim@anche.no>
2019-05-11 21:09:00 +0200

    some structure and file cleaning, starting admin.php

--- admin.php
+++ admin.php
@@ -2,7 +2,7 @@
 <html lang='it'>
   <head>
     <!-- Required meta tags -->
-    <title>TornELO</title>
+    <title>Admin | TornELO</title>
     <meta charset='utf-8'>
 	<meta name="description" content="TornELO is a self-hosted opensource tournament server and script" />
     <meta name='viewport' content='width=device-width, initial-scale=1, shrink-to-fit=no'>
@@ -10,8 +10,8 @@
     <link rel='icon' href='img/antipong_favicon.png'>
     <link rel="stylesheet" href="css/main.css" />
     <meta name="HandheldFriendly" content="true" />
-	<meta name="mobile-web-app-capable" content="yes">    
-
+	<meta name="mobile-web-app-capable" content="yes">
+    
     <body>
         <center>
 
@@ -42,70 +42,8 @@
 
 
 
-            <img id='qr' src='img/torneloQR.gif' height='140px' width='140px' alt='http://flowin.space/tornelo/' />
+            <!-- <img id='qr' src='img/torneloQR.gif' height='140px' width='140px' alt='http://flowin.space/tornelo/' /> -->
 
         </center>
     </body>
 </html>
-
-<?php
-    // echo $output2
-
-# get a json and dump
-// $json = file_get_contents('data/ping/ping.json');  
-// $json_output = json_decode($json, true); 
-// echo $json_output;
-
-// foreach ($json_output as $trend){         
-//    echo $trend['text']."\n";     
-//   } 
-
-
-# trasforma il dizionario python in un array php
-// $data =  json_decode($output, true);
-// print_r($data);
-    // if (count($data->stand)) {
-    //     // Open the table
-    //     echo "<table>";
-
-    //     // Cycle through the array
-    //     foreach ($data->stand as $idx => $stand) {
-
-    //         // Output a row
-    //         echo "<tr>";
-    //         echo "<td>$stand->afko</td>";
-    //         echo "<td>$stand->positie</td>";
-    //         echo "</tr>";
-    //     }
-
-    //     // Close the table
-    //     echo "</table>";
-    // }
-
-    // function build_table($array){
-    //     // start table
-    //     $html = '<table>';
-    //     // header row
-    //     $html .= '<tr>';
-    //     foreach($array[0] as $key=>$value){
-    //             $html .= '<th>' . htmlspecialchars($key) . '</th>';
-    //         }
-    //     $html .= '</tr>';
-
-    //     // data rows
-    //     foreach( $array as $key=>$value){
-    //         $html .= '<tr>';
-    //         foreach($value as $key2=>$value2){
-    //             $html .= '<td>' . htmlspecialchars($value2) . '</td>';
-    //         }
-    //         $html .= '</tr>';
-    //     }
-
-    //     // finish table and return it
-
-    //     $html .= '</table>';
-    //     return $html;
-    // }
-
-    // echo build_table($data);
-?>
