78cfb51ec880a2f5c702445218a3a8fb84493da5
sbiego <forsesim@anche.no>
2019-05-30 00:55:25 +0200

    better division js/html and fix menu icon click bug

--- js/script.js
+++ js/script.js
@@ -5,7 +5,9 @@ function toggleTheme() {
     // tabelle
     $('table').toggleClass('table-dark');
     $('tr.success').toggleClass('bg-success');
-    $('tr.success').toggleClass('table-success');    
+    $('tr.success').toggleClass('table-success');
+
+    console.log('changing theme...');
 }
 
 function getUrlVars() {
@@ -16,9 +18,33 @@ function getUrlVars() {
     return vars;
 }
 
-/////////// Auto NIGHT MODE ///////////////////
-window.onload = function () {
 
+
+window.onload = function () {
+    
+    /////////// BINDING ///////////////////////////
+    // theme / logo
+    $('#logo').bind('click', function(){
+        toggleTheme();
+        console.log('Logo clicked, changing theme...')
+    });
+    
+    // open menu
+    $('#admin_button').bind('click', function(){
+        $('#menu').slideToggle();
+        $('#admin_icon').toggleClass('active')
+        console.log('Menu button clicked, toggling menu...')
+    });
+
+    // close menu (when active) on outside click
+    $('#content').bind('click', function() {
+        if ($('#menu').css('display') != 'none') {
+            $('#menu').slideUp();
+            $('#admin_icon').toggleClass('active');
+        }
+    });
+    
+    /////////// Auto NIGHT MODE ///////////////////
     var urlVars = getUrlVars();
     var d = new Date();
     var hour = d.getHours();
