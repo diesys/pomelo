886d3c8a8c50b40f2c77f4d4f72839858eb4e1cb
sbiego <forsesim@anche.no>
2019-05-30 09:51:38 +0200

    better torneo selector button

--- js/script.js
+++ js/script.js
@@ -51,6 +51,120 @@ window.onload = function () {
             $('#admin_icon').toggleClass('active');
         }
     });
+
+
+    // select tornei
+    $('#buttonSingolo').bind('click', function() {
+        if ($('#singoloRanking').is(':visible') | $('#singoloPartite').is(':visible')) {
+            $(this).removeClass('active');
+            $('.buttonSingolo').removeClass('active');
+            $('.collapse-singolo').fadeOut();
+        } else {
+            $(this).addClass('active');
+            $('.buttonSingolo').addClass('active');
+            $('.collapse-singolo').fadeIn();
+        } 
+    });
+    
+    $('#buttonRankSingolo').bind('click', function() {
+        bS = $('#buttonSingolo').hasClass('active');
+        bPS = $('#buttonPartSingolo').hasClass('active');
+        bRS = $('#buttonRankSingolo').hasClass('active');
+        
+        if ( bS && bPS && bRS ) {
+            $('#singoloRanking').fadeOut();
+            $(this).removeClass('active');
+        } else if ( bS && bPS ) {
+            $('#singoloRanking').fadeIn();
+            $(this).addClass('active');
+        } else if ( bS ) {
+            $('#singoloRanking').fadeOut();
+            $(this).removeClass('active');
+            $('#buttonSingolo').removeClass('active');
+        } else {
+            $('#singoloRanking').fadeIn();
+            $(this).addClass('active');
+            $('#buttonSingolo').addClass('active');
+        }        
+    });
+    
+    $('#buttonPartSingolo').bind('click', function() {
+        bS = $('#buttonSingolo').hasClass('active');
+        bPS = $('#buttonPartSingolo').hasClass('active');
+        bRS = $('#buttonRankSingolo').hasClass('active');
+
+        if (bS && bPS && bRS) {
+            $('#singoloPartite').fadeOut();
+            $(this).removeClass('active');
+        } else if (bS && bRS) {
+            $('#singoloPartite').fadeIn();
+            $(this).addClass('active');
+        } else if (bS) {
+            $('#singoloPartite').fadeOut();
+            $(this).removeClass('active');
+            $('#buttonSingolo').removeClass('active');
+        } else {
+            $('#singoloPartite').fadeIn();
+            $(this).addClass('active');
+            $('#buttonSingolo').addClass('active');
+        }   
+    });
+
+    $('#buttonDoppio').bind('click', function() {
+        if ($('#doppioRanking').is(':visible') | $('#doppioPartite').is(':visible')) {
+            $(this).removeClass('active');
+            $('.buttonDoppio').removeClass('active');
+            $('.collapse-doppio').fadeOut();
+        } else {
+            $(this).addClass('active');
+            $('.buttonDoppio').addClass('active');
+            $('.collapse-doppio').fadeIn();
+        } 
+    });
+
+    $('#buttonRankDoppio').bind('click', function () {
+        bS = $('#buttonDoppio').hasClass('active');
+        bPS = $('#buttonPartDoppio').hasClass('active');
+        bRS = $('#buttonRankDoppio').hasClass('active');
+
+        if (bS && bPS && bRS) {
+            $('#doppioRanking').fadeOut();
+            $(this).removeClass('active');
+        } else if (bS && bPS) {
+            $('#doppioRanking').fadeIn();
+            $(this).addClass('active');
+        } else if (bS) {
+            $('#doppioRanking').fadeOut();
+            $(this).removeClass('active');
+            $('#buttonDoppio').removeClass('active');
+        } else {
+            $('#doppioRanking').fadeIn();
+            $(this).addClass('active');
+            $('#buttonDoppio').addClass('active');
+        }
+    });
+
+    $('#buttonPartDoppio').bind('click', function () {
+        bS = $('#buttonDoppio').hasClass('active');
+        bPS = $('#buttonPartDoppio').hasClass('active');
+        bRS = $('#buttonRankDoppio').hasClass('active');
+
+        if (bS && bPS && bRS) {
+            $('#doppioPartite').fadeOut();
+            $(this).removeClass('active');
+        } else if (bS && bRS) {
+            $('#doppioPartite').fadeIn();
+            $(this).addClass('active');
+        } else if (bS) {
+            $('#doppioPartite').fadeOut();
+            $(this).removeClass('active');
+            $('#buttonDoppio').removeClass('active');
+        } else {
+            $('#doppioPartite').fadeIn();
+            $(this).addClass('active');
+            $('#buttonDoppio').addClass('active');
+        }
+    });
     
     /////////// Auto NIGHT MODE ///////////////////
     var urlVars = getUrlVars();
